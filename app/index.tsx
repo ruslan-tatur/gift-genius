94a4614db0e58dd1343eced55bd8062c0094abe7
Ruslan <ruslan.tatur@gmail.com>
2025-02-21 10:57:13 +0200

    Added logo images

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,33 @@
-import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import SplashScreen from "@/components/splash-screen";
+import { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import StyledText from '@/components/styled-text';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function Index() {
-  const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 10000);
   }, []);
-
-  if (true) {
-    return <SplashScreen />;
-  }
 
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <StyledText>Edit app/index.tsx to edit this screen.</StyledText>
     </View>
   );
 }
